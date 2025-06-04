c9c5048bfda60d0a96a909fde34f044168180775
sibou <sibou@example.com>
2025-06-04 11:40:24 +0100

    schema fix

--- api/schemas/process_schemas.py
+++ api/schemas/process_schemas.py
@@ -24,19 +24,3 @@ class ProcessAnalysisResult(BaseModel):
     record_id: int
     is_anomaly: bool
     anomaly_probability: float
-    ts: Optional[int]
-    CMD: Optional[str]
-    TRUN: Optional[float]  # Changed to float
-    TSLPI: Optional[int]
-    TSLPU: Optional[int]
-    EXC: Optional[int]
-    CPU: Optional[float]
-    NICE: Optional[int]
-    PRI: Optional[int]
-    CPUNR: Optional[int]
-    POLI: Optional[str]
-    Status: Optional[str]
-    State: Optional[str]
-    PID: Optional[int]
-    UID: Optional[int]
-    USER: Optional[str]
